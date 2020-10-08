16d904b791b1c87b9bb881eb0679a4657182aee1
Alexey Makano <almakano@gmail.com>
2020-10-08 19:26:39 +0300

    Create install_web.sh

---
+++ install_web.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+	echo "Usage: ./${0##*/} mysql_admin_user_name"
+	exit 1;
+fi
+
+echo "Install console benefits"
+apt install -y curl wget gcc g++ make bash-completion net-tools easy-rsa git mc htop ngrep tcpdump rsync mtr ipset lm-sensors libmemcached-dev gnupg2 ca-certificates lsb-release python-software-properties
+
+echo "Install nginx"
+echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list; apt update; apt install nginx-full
+
+echo "Install apache"
+apt install -y apache2
+
+echo "Install php7.4"
+add-apt-repository ppa:ondrej/php; apt update; apt install -y php7.4 php7.4-fpm php7.4-mbstring php7.4-curl
+
+echo "Install mysql memcached"
+apt install -y mysql-server-5.7 memcached
+echo "Add mysql user $1"
+echo "GRANT ALL PRIVILEGES ON *.* TO '$1'@'%' IDENTIFIED BY '$1' WITH GRANT OPTION; flush privileges;" | mysql 
+
+echo "Install exim4"
+apt install -y exim4 bsd-mailx
+
+echo "Install certbot"
+add-apt-repository ppa:certbot/certbot; apt update
+apt install -y certbot python-certbot-nginx
+
+echo "Install nodejs"
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
+
+exit 0;
