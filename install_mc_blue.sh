6a20c0ca35a462ce0925f29c3aa438d5db52947c
Alexey Makano <almakano@gmail.com>
2021-01-04 19:47:47 +0200

    Update install_mc_blue.sh

--- install_mc_blue.sh
+++ install_mc_blue.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cp /usr/share/mc/skins
-wget https://raw.githubusercontent.com/almakano/devops/main/usr/share/mc/skins/blue.ini
+if [ ! -f "/usr/share/mc/skins/blue.ini" ]; then
+  wget https://raw.githubusercontent.com/almakano/devops/main/usr/share/mc/skins/blue.ini -O /usr/share/mc/skins/blue.ini
+fi 
 sed -i 's/skin=default/skin=blue/' ~/.config/mc/ini
 
 exit 0;
