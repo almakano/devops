10e2c8b917a1d5b2703313ebfb4875aaa7ffa5d7
Alexey Makano <almakano@gmail.com>
2021-01-11 13:36:45 +0200

    Update install_ipset.sh

--- install_ipset.sh
+++ install_ipset.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-apt install ipset iptables-persistent
+if [ -z "$(dpkg -l | grep 'ipset')" ]; then 
+    apt install ipset iptables-persistent
+fi
 
 if [ ! -d /etc/ipset ]; then mkdir /etc/ipset; fi
 if [ ! -d /etc/iptables ]; then mkdir /etc/iptables; fi
@@ -19,16 +21,24 @@ if [ -z "$(grep -iRI 'exit 0' /etc/rc.local)" ]; then
     echo "exit 0" >> /etc/rc.local
 fi
 
+iptables-save | grep '/32 -j REJECT' | awk '{print $4}' | sort >> /etc/ipset/blacklist_auth.txt
+
 echo "create whitelist_ip4 hash:net family inet hashsize 4096 maxelem 800000" > /etc/ipset/rules
 sed 's/^/add whitelist_ip4 /' /etc/ipset/whitelist*.txt >> /etc/ipset/rules
 echo "create blacklist_ip4 hash:net family inet hashsize 4096 maxelem 800000" >> /etc/ipset/rules
 sed 's/^/add blacklist_ip4 /' /etc/ipset/blacklist*.txt >> /etc/ipset/rules
 
 ipset restore -! < /etc/ipset/rules
+iptables-restore < /etc/iptables/rules
 
-iptables -A INPUT -m set --match-set whitelist_ip4 src -j ACCEPT
-iptables -A INPUT -m set --match-set blacklist_ip4 src -j LOG --log-prefix "Iptables: DROP ip4 "
-iptables -A INPUT -m set --match-set blacklist_ip4 src -j DROP
+if [ -z "$(grep -iRI 'whitelist_ip4' /etc/iptables/rules)" ]; then 
+    iptables -A INPUT -m set --match-set whitelist_ip4 src -j ACCEPT
+fi
+
+if [ -z "$(grep -iRI 'blacklist_ip4' /etc/iptables/rules)" ]; then 
+    iptables -A INPUT -m set --match-set blacklist_ip4 src -j LOG --log-prefix "Iptables: DROP ip4 "
+    iptables -A INPUT -m set --match-set blacklist_ip4 src -j DROP
+fi
 
 iptables-save > /etc/iptables/rules
 
