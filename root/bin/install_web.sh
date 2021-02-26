52e6e7933118fbc38f535e026f214806f2ba7818
Alexey Makano <almakano@gmail.com>
2021-02-26 16:50:21 +0200

    Update install_web.sh

--- root/bin/install_web.sh
+++ root/bin/install_web.sh
@@ -5,6 +5,8 @@ if [[ -z "$1" ]]; then
 	exit 1;
 fi
 
+dpkg-reconfigure tzdata
+
 echo "Install console benefits"
 apt install -y curl wget gcc g++ git make bash-completion net-tools htop ngrep tcpdump mtr ipset easy-rsa mc rsync lm-sensors
 apt instal -y gnupg2 ca-certificates lsb-release python-software-properties
@@ -23,7 +25,7 @@ add-apt-repository ppa:ondrej/php; apt update;
 apt install -y php7.4 php7.4-fpm php7.4-mbstring php7.4-curl php7.4-mysql php7.4-opcache php7.4-gd php7.4-imagick php7.4-xml php7.4-zip
 
 echo "Install apache"
-apt install -y apache2 libapache2-mpm-itk libapache2-mod-ruid2 libapache2-mod-rpaf libapache2-mod-php8.0 libapache2-mod-php7.4
+apt install -y apache2 libapache2-mpm-itk libapache2-mod-ruid2 libapache2-mod-rpaf libapache2-mod-php7.4
 a2enmod -q alias; a2enmod -q actions; a2enmod -q cgi; a2enmod -q mpm_prefork; a2enmod -q remote_ip; a2enmod -q rewrite; a2enmod -q ruid2; a2enmod -q setenvif; a2enmod -q vhost_alias; 
 a2dismod -q ssl
 wget https://raw.githubusercontent.com/almakano/devops/main/etc/apache2/apache2.conf -O /etc/apache2/apache2.conf
@@ -40,10 +42,11 @@ fi
 echo "Install mysql memcached"
 apt install -y mysql-server-8.0 memcached libmemcached-dev
 echo "Add mysql user $1"
-echo "GRANT ALL PRIVILEGES ON *.* TO '$1'@'%' IDENTIFIED BY '$1' WITH GRANT OPTION; flush privileges;" | mysql 
+echo "CREATE USER '$1'@'%' IDENTIFIED BY '$1'; GRANT ALL PRIVILEGES ON *.* TO '$1'@'%' WITH GRANT OPTION; flush privileges;" | mysql 
 
 echo "Install exim4"
 apt install -y exim4 bsd-mailx
+dpkg-reconfigure exim4-config
 
 echo "Install nodejs npm"
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
