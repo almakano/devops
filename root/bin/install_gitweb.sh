2663a5de083941a1c9a747a55323367660d68d34
Alexey Makano <almakano@gmail.com>
2021-03-04 20:59:54 +0200

    Update install_gitweb.sh

--- root/bin/install_gitweb.sh
+++ root/bin/install_gitweb.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+read -p "Enter domain: " domain
+
 git clone git://git.kernel.org/pub/scm/git/git.git
 cd git/
 make GITWEB_PROJECTROOT="/opt/git" prefix=/usr gitweb
@@ -10,6 +12,9 @@ rm -rf git/
 mkdir -p /var/www/gitweb/conf
 wget https://raw.githubusercontent.com/almakano/devops/main/var/www/gitweb/conf/apache.conf -O /var/www/gitweb/conf/apache.conf
 wget https://raw.githubusercontent.com/almakano/devops/main/var/www/gitweb/conf/nginx.conf -O /var/www/gitweb/conf/nginx.conf
+
+sed -i "s/#DOMAINNAME#/$domain/g" /var/www/gitweb/conf/apache.conf
+sed -i "s/#DOMAINNAME#/$domain/g" /var/www/gitweb/conf/nginx.conf
 
 service nginx configtest && service nginx restart
 apachectl configtest && service apache2 restart
