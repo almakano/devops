459dd27afb60df180c8cce6e8aff9232367cd76a
Alexey Makano <almakano@gmail.com>
2021-05-31 17:04:31 +0300

    Update update.sh

--- etc/ipset/update.sh
+++ etc/ipset/update.sh
@@ -3,8 +3,12 @@
 iptables-save | grep '/32 -j REJECT' | awk '{print $4}' | sort | uniq >> /etc/ipset/blacklist_auth.txt
 iptables-restore < /etc/iptables/rules
 cat /etc/ipset/blacklist_auth.txt | sort | uniq > /etc/ipset/blacklist_auth2.txt
-rm /etc/ipset/blacklist_auth.txt
-mv /etc/ipset/blacklist_auth2.txt /etc/ipset/blacklist_auth.txt
+rm /etc/ipset/blacklist_auth.txt; mv /etc/ipset/blacklist_auth2.txt /etc/ipset/blacklist_auth.txt
+
+zcat /home/*/www/*/logs/*log*.gz | grep '0x%5B%5D=' | awk '{print $2}' | sort | uniq >> /etc/ipset/blacklist_web.txt;
+cat /home/*/www/*/logs/*.log | grep '0x%5B%5D=' | awk '{print $2}' | sort | uniq >> /etc/ipset/blacklist_web.txt;
+cat /etc/ipset/blacklist_web.txt | sort | uniq >> /etc/ipset/blacklist_web2.txt
+rm /etc/ipset/blacklist_web.txt; mv /etc/ipset/blacklist_web2.txt /etc/ipset/blacklist_web.txt
 
 echo "create whitelist_ip4 hash:net family inet hashsize 4096 maxelem 800000" > /etc/ipset/rules
 sed 's/^/add whitelist_ip4 /' /etc/ipset/whitelist*.txt >> /etc/ipset/rules
